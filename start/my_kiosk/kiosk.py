9f49cdbf3d9cf92abaa7218c83ef0ab2f88ae331
tmdstart <tmdstart@gmail.com>
2025-06-12 12:47:35 +0900

    test ui

--- start/my_kiosk/kiosk.py
+++ start/my_kiosk/kiosk.py
@@ -1,6 +1,22 @@
 import time
 from function import display_cart, select_pro, pay_deci, end_screen_delay, pay_screen
 
+# 전체 메뉴를 출력하는 함수 
+def display_menu():
+    print('*' * 40)
+    print('어서오세요, ‘요렇게’에 오신 것을 환영합니다:)')
+    print("Menu\t\t\t\tPrice")
+    print("-" * 30)
+    print("요거트 아이스크림(250g)\t3,000")
+    print("-" * 30)
+    print("\nNo. Topping\t\tPrice")
+    print("-" * 30)
+    for idx, item in enumerate(menu_list, start=1):
+        print(f"{idx:<5} {item['name']:<10}\t{item['price']:,}")
+    print("-" * 30)
+    print("*" * 40) 
+
+
 # 멤버십 포인트 정보를 저장하는 딕셔너리
 membership_dict = {}
 
